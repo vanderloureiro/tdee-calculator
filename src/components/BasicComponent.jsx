8fc09381b2332f1114e3adb8e37f485457ad8597
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2020-11-19 22:19:54 -0300

    Basic layout

---
+++ src/components/BasicComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class BasicComponent extends React.Component {
+
+    render() {
+        return <div>
+            <h2>Basic component</h2>
+        </div>
+    }
+}
---
+++ src/components/Description.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Description.css';
+
+export default class Description extends React.Component {
+
+    render() {
+        return <div className="description">
+            <h3>Cálculo de gastro calórico</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Deserunt possimus similique debitis rerum ipsa labore beatae aliquid, 
+                blanditiis voluptates culpa rem consequuntur exercitationem provident velit facilis temporibus quos. 
+                Soluta, eos.</p>
+        </div>
+    }
+}
